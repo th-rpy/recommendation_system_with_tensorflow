15922e8c52e3a50b5dfc1e905a2ce5cbbe98c05e
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-20 21:50:33 -0400

    Add model.py file

---
+++ two_towers_model.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Import the libraires
+from typing import Dict, Text
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+import tensorflow_recommenders as tfrs
+import pprint
+from matplotlib import pyplot as plt
+
+    ############################
+

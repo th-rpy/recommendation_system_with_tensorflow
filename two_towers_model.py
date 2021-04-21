04560d089375b1b5ec24e11122887d010d6d1076
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-20 21:52:58 -0400

    Loading the dataset

--- two_towers_model.py
+++ two_towers_model.py
@@ -13,3 +13,17 @@ from matplotlib import pyplot as plt
 
     ############################
 
+# Load data for votes
+votes = tfds.load("movielens/100k-ratings", split="train")
+# Load data for movies
+movies = tfds.load("movielens/100k-movies", split="train")
+
+# We need to map the necessary attributes
+ 
+votes = votes.map(lambda x: {"movie_title": x["movie_title"], "user_id": x["user_id"],})
+movies = movies.map(lambda x: x['movie_title'])      
+
+#View the data
+
+for x in votes.take(1).as_numpy_iterator():
+    pprint.pprint(x)

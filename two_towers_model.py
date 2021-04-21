0198e27b89728424f6da90d0b1caf472178cf4d2
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-20 21:58:25 -0400

    update model.py file

--- two_towers_model.py
+++ two_towers_model.py
@@ -27,3 +27,35 @@ movies = movies.map(lambda x: x['movie_title'])
 
 for x in votes.take(1).as_numpy_iterator():
     pprint.pprint(x)
+
+# We separate our data set into training/testing
+# Assign a seed=73 for consistency of results and permtuez the data to keep no particular order
+seed = 73
+    ## Dataset length
+l = len(votes)
+tf.random.set_seed(seed)
+shuffled = votes.shuffle(l, seed=seed, reshuffle_each_iteration=False)     
+
+#Save 75% of the data for training and 25% for testing
+train_ = int(0.75 * l)
+test_ = int(0.25 * l)
+train = shuffled.take(train_)
+test = shuffled.skip(train_).take(test_) 
+
+# We check how many users and unique movies     
+movies_titles = movies.batch(l)
+user_ids = votes.batch(l).map(lambda x: x['user_id'])
+
+#films uniques
+'''titles = iter(films_titres)
+titles = list(set(next(titles).numpy()))'''
+titles = np.unique(np.concatenate(list(films_titres)))
+len_films = len(titles)
+print(len_films) #1682
+
+    #users unique
+'''ids = iter(user_ids)
+ids_ = list(set(next(ids).numpy()))'''
+ids = np.unique(np.concatenate(list(user_ids)))
+len_users = len(ids)
+print(len_users) #943
